9fedeb54abfa7b55e40a8c802de444e682d6acd5
David Wolter <david@lovoo.com>
2015-05-20 18:32:06 +0200

    add enabled & environment voters

--- src/Dwo/FlaggingBundle/Tests/Functional/FlaggingVotersTest.php
+++ src/Dwo/FlaggingBundle/Tests/Functional/FlaggingVotersTest.php
@@ -2,6 +2,7 @@
 
 namespace Dwo\FlaggingBundle\Tests\Functional;
 
+use Dwo\Flagging\Context\Context;
 use Dwo\Flagging\Exception\FlaggingException;
 use Dwo\Flagging\Model\VoterManagerInterface;
 use Dwo\FlaggingBundle\Tests\DependencyInjection;
@@ -23,33 +24,55 @@ class FlaggingVotersTest extends AbstractContainerTestCase
     }
 
     /**
-     * @dataProvider provider
+     * @dataProvider providerVoters
      */
     public function testGetVoter($voterName, $null = false)
     {
         try {
             $voter = self::$manager->getVoter($voterName);
-        }
-        catch(FlaggingException $e) {
+        } catch (FlaggingException $e) {
             $voter = null;
         }
 
-        if($null) {
+        if ($null) {
             self::assertNull($voter);
-        }
-        else {
+        } else {
             self::assertNotNull($voter);
         }
     }
 
-    public function provider() {
+    /**
+     * @dataProvider providerEnvironment
+     */
+    public function testEnvironmentVoter($result, $config)
+    {
+        $voter = self::$manager->getVoter('environment');
+
+        self::assertEquals($result, $voter->vote($config, new Context()));
+    }
+
+    public function providerVoters()
+    {
         return array(
             array('name'),
             array('expression'),
             array('date_range'),
             array('random'),
             array('feature'),
+            array('enabled'),
+            array('environment'),
             array('foo', true),
+        );
+    }
+
+    public function providerEnvironment()
+    {
+        return array(
+            array(true, 'test'),
+            array(true, ['test']),
+            array(true, ['prod', 'test']),
+            array(false, 'prod'),
+            array(false, ['prod', 'dev']),
         );
     }
 }
